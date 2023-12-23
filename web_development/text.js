5cf323ce7f8fee36a3032d91b742bd6591409f85
immrkumarrohit <142918146+immrkumarrohit@users.noreply.github.com>
2023-12-23 17:22:01 +0530

    Update text.js

--- web_development/text.js
+++ web_development/text.js
@@ -1,1 +1,8 @@
 console.log("Rohit");
+//  Q =>  We are given array of marks of students filter out of the marks of students that scored 90+
+
+let arr = [85,96,81,89,99,92,78,88,95,90,93];
+let scoreHigh = arr.filter((score)=>{
+  return score > 90 ;
+})
+console.log(scorehigh);
